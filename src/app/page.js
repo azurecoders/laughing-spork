dcc6c1ccce7e44ab1e01dd4cf87df600c93548e2
Muzammil Ahmed <166981810+azurecoders@users.noreply.github.com>
2024-12-15 10:36:29 +0500

    INITIAL COMMIT

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,30 @@
-import Image from "next/image";
+import { FetchUsersAction } from "@/actions";
+import AddNewUser from "@/components/add-new-user";
+import SingleUserCard from "@/components/single-user-card";
+import React from "react";
 
-export default function Home() {
+const UserManagement = async () => {
+  const getListOfUsers = await FetchUsersAction();
+
   return (
-    <div>
-      <h1>Server Actions</h1>
+    <div className="p-20 max-w-6xl mx-auto">
+      <div className="flex justify-between items-center">
+        <h1 className="text-3xl font-semibold">User Management</h1>
+        <AddNewUser />
+      </div>
+      <div className="mt-6 grid grid-cols-1 md:grid-cols-3 gap-5">
+        {getListOfUsers &&
+        getListOfUsers.data &&
+        getListOfUsers.data.length > 0 ? (
+          getListOfUsers.data.map((user) => (
+            <SingleUserCard key={user._id} user={user} />
+          ))
+        ) : (
+          <h3>No Users Found</h3>
+        )}
+      </div>
     </div>
   );
-}
+};
+
+export default UserManagement;
